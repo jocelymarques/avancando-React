ffef3ef5c65e2c2d5651447fa69fac6dfa6d44b3
jocelymarques <fulanodetal@exemplo.br>
2022-06-19 21:45:55 -0300

    props parameters

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ConditionalRender from './assets/Components/ConditionalRender';
 import ListRender from './assets/Components/ListRender';
 import ManageData from './assets/Components/ManageData';
 import ShowUserName from './assets/Components/ShowUserName';
+import CarDetails from './assets/Components/CarDetails';
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
       <ListRender />
       <ConditionalRender/>
       <ShowUserName name={userName} />
+      <CarDetails brand="Ford" km={10000} color="Azul"></CarDetails>
 
     </>
 
